fbcf9fd2bcf6d3575b73a5d40c4cf10419fc466c
Senpai_Desi <72793345+SenpaiDesi@users.noreply.github.com>
2022-09-05 05:44:31 +0200

    Update assets.py
    Added statuses

--- assets.py
+++ assets.py
@@ -1,7 +1,11 @@
 config_path = "./jsonfiles/config.json"
 Database_file = "./database.db"
 
-statuses = ["test"]
+statuses = ["https://youtube.com/channel/UCVUAQQwGVhpTKzg5ivlR58A",
+            "https://codechaos.net",
+            "Watching funny content",
+            "Watching you!",
+            "Guarding our servers"]
 extensions = ["admins.admins",
               "help.help",
               "moderation.moderation"]
